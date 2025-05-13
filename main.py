214d7b442a6f379e638adce39b40322309c9edbd
Adam Levinson <adamlev513@gmail.com>
2025-05-13 05:02:00 +0300

    refactor: replace input() with argparse in main.py

--- main.py
+++ main.py
@@ -1,42 +1,40 @@
+import argparse
 import boto3
 import ec2
 import s3
 import route53
+import cloudwatch
 
-access_key = input("Enter your AWS Access Key ID: ")
-secret_key = input("Enter your AWS Secret Access Key: ")
-default_region = 'us-east-1'
-region_name = input(f"Enter the AWS region (press enter for default {default_region}): ") or default_region
+def create_session(access_key, secret_key, region):
+    return boto3.Session(
+        aws_access_key_id=access_key,
+        aws_secret_access_key=secret_key,
+        region_name=region
+    )
 
-# Create a session
-session = boto3.Session(
-    aws_access_key_id=access_key,
-    aws_secret_access_key=secret_key,
-    region_name=region_name
-)
+def main():
+    parser = argparse.ArgumentParser(description="AWS CLI Tool with argparse")
+    parser.add_argument('--service', required=True, choices=['ec2', 's3', 'route53', 'cloudwatch'], help='Service to use')
+    parser.add_argument('--access_key', required=True, help='AWS Access Key ID')
+    parser.add_argument('--secret_key', required=True, help='AWS Secret Access Key')
+    parser.add_argument('--region', default='us-east-1', help='AWS Region')
+    parser.add_argument('--action', required=False, help='Optional action (used in cloudwatch)')
 
-# Create clients for the AWS services
-ec2_client = session.client('ec2')
-s3_client = session.client('s3')
-route53_client = session.client('route53')
+    args = parser.parse_args()
 
-# Main menu
-while True:
-    print("Choose a service:")
-    print("1. EC2")
-    print("2. S3")
-    print("3. Route53")
-    print("4. Exit")
-    choice = input("Enter your choice: ")
+    session = create_session(args.access_key, args.secret_key, args.region)
 
-    if choice == "1":
+    if args.service == 'ec2':
+        ec2_client = session.client('ec2')
         ec2.main(ec2_client)
-    elif choice == "2":
+    elif args.service == 's3':
+        s3_client = session.client('s3')
         s3.main(s3_client)
-    elif choice == "3":
+    elif args.service == 'route53':
+        route53_client = session.client('route53')
         route53.handle_route53(route53_client)
-    elif choice == "4":
-        print("Goodbye!")
-        break
-    else:
-        print("Invalid choice.")
+    elif args.service == 'cloudwatch':
+        cloudwatch.main(args.access_key, args.secret_key, args.region, args.action)
+
+if __name__ == "__main__":
+    main()
